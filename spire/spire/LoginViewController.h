cb45559c1e040a6ced19a229e97f0411208a221b
Lucy Guo <lguo@andrew.cmu.edu>
2014-07-19 12:06:43 -0700

    view controller

---
+++ spire/spire/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  spire
+//
+//  Created by Lucy Guo on 7/19/14.
+//  Copyright (c) 2014 Niveditha Jayasekar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end

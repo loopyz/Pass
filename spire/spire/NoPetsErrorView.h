f42942539032e4fc3bdf70400a0327d0d62a716a
loopyz <lguo@andrew.cmu.edu>
2014-07-20 01:13:37 -0700

    no pets error

---
+++ spire/spire/NoPetsErrorView.h
@@ -0,0 +1,13 @@
+//
+//  NoPetsErrorView.h
+//  spire
+//
+//  Created by Lucy Guo on 7/20/14.
+//  Copyright (c) 2014 Niveditha Jayasekar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NoPetsErrorView : UIView
+
+@end

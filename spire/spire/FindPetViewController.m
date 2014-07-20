4981c74c766894c908e2b040b9367e5414da84ce
Niveditha Jayasekar <jnive05@gmail.com>
2014-07-20 00:57:47 -0700

    trying to make section headers for findpetctrl

--- spire/spire/FindPetViewController.m
+++ spire/spire/FindPetViewController.m
@@ -8,17 +8,20 @@
 
 #import "FindPetViewController.h"
 
+#define SCREEN_WIDTH ((([UIApplication sharedApplication].statusBarOrientation == UIInterfaceOrientationPortrait) || ([UIApplication sharedApplication].statusBarOrientation == UIInterfaceOrientationPortraitUpsideDown)) ? [[UIScreen mainScreen] bounds].size.width : [[UIScreen mainScreen] bounds].size.height)
+#define SCREEN_HEIGHT ((([UIApplication sharedApplication].statusBarOrientation == UIInterfaceOrientationPortrait) || ([UIApplication sharedApplication].statusBarOrientation == UIInterfaceOrientationPortraitUpsideDown)) ? [[UIScreen mainScreen] bounds].size.height : [[UIScreen mainScreen] bounds].size.width)
+
 @interface FindPetViewController ()
 
 @end
 
 @implementation FindPetViewController
-
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
+        [self setupPetCollection];
     }
     return self;
 }
@@ -26,7 +29,95 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+}
+
+- (void) setupPetCollection
+{
+    UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc] init];
+    layout.minimumInteritemSpacing = 2;
+    layout.minimumLineSpacing = 2;
+    layout.sectionInset = UIEdgeInsetsMake(20, 0, 20, 0);
+    layout.scrollDirection = UICollectionViewScrollDirectionVertical;
+    layout.headerReferenceSize = CGSizeMake(SCREEN_WIDTH, 50);
+
+    
+    self.collectionView = [[UICollectionView alloc] initWithFrame:CGRectMake(0,0, SCREEN_WIDTH, 750) collectionViewLayout:layout];
+    [self.collectionView setDataSource:self];
+    [self.collectionView setDelegate:self];
+    [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"petCell"];
+    [self.collectionView setBackgroundColor:[UIColor lightGrayColor]];
+    
+    [self.collectionView setAllowsSelection:NO];
+}
+
+- (NSInteger) numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+    return 3;
+}
+
+- (NSInteger) collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 6;
+}
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    return CGSizeMake(80, 100);
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionViewCell *cell;
+    cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"petCell" forIndexPath:indexPath];
+
+    cell.backgroundColor = [UIColor clearColor];
+    
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 70, 70)];
+    
+    imageView.image = [UIImage imageNamed:@"bunny.png"];
+    float width = imageView.bounds.size.width;
+    imageView.layer.cornerRadius = width/2;
+    [cell addSubview:imageView];
+    
+    UIColor *descColor = [UIColor blackColor];
+    
+    //setup Location label
+    UILabel *desc = [[UILabel alloc] initWithFrame:CGRectMake(0, 70, 70, 50)];
+    [desc setTextColor:descColor];
+    [desc setBackgroundColor:[UIColor clearColor]];
+    [desc setFont:[UIFont fontWithName:@"Avenir" size:11]];
+    
+    desc.text = @"Mountain View, CA";
+    desc.lineBreakMode = NSLineBreakByWordWrapping;
+    desc.numberOfLines = 0;
+    [cell addSubview:desc];
+    
+    return cell;
+}
+
+- (UICollectionReusableView *)collectionView:(UICollectionView *)collectionView viewForSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionReusableView *reusableView = nil;
+    
+    if (kind == UICollectionElementKindSectionHeader) {
+        reusableView = [collectionView dequeueReusableSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:@"headerView" forIndexPath:indexPath];
+        UILabel *textLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, 100)];
+        
+        if (indexPath.section == 0) {
+            textLabel.text = @"Here - 500 feet";
+        } else if (indexPath.section == 1) {
+            textLabel.text = @"A Walk Away - 1.0 miles";
+        } else {
+            textLabel.text = @"A Drive Away - 10.0 miles";
+        }
+        [reusableView addSubview:textLabel];
+    }
+    if (kind == UICollectionElementKindSectionFooter) {
+        reusableView = [collectionView dequeueReusableSupplementaryViewOfKind:UICollectionElementKindSectionFooter withReuseIdentifier:@"footerView" forIndexPath:indexPath];
+    }
+    
+    return reusableView;
 }
 
 - (void)didReceiveMemoryWarning

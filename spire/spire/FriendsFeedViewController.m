cc2090e01ea5d05784dfa03a7b232696e0118083
Niveditha Jayasekar <jnive05@gmail.com>
2014-07-20 09:17:36 -0700

    notifications ctrl done

--- spire/spire/FriendsFeedViewController.m
+++ spire/spire/FriendsFeedViewController.m
@@ -37,6 +37,20 @@
   return self;
 }
 
+- (NSString *)getAction
+{
+    NSArray* types = @[@"commented on your photo.", @"followed you.", @"accepted your friend request."];
+    
+    return types[arc4random() % [types count]];
+}
+
+- (NSString *)getPerson
+{
+    NSArray* types = @[@"andrewhuang", @"charlotte", @"keithmiller", @"lucyguo", @"nivejayasekar", @"vivianma"];
+    
+    return types[arc4random() % [types count]];
+}
+
 - (void)viewDidLoad
 {
   [super viewDidLoad];
@@ -46,29 +60,9 @@
   self.ptr = [[PullToRefresh alloc] initWithNumberOfDots:5];
   self.ptr.delegate = self;
   [self.view addSubview:self.ptr];
-  PFQuery *query = [PFQuery queryWithClassName:@"Video"];
-  [query orderByDescending:@"createdAt"];
-  [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
-    if (objects) {
-      self.videos = objects;
-      [self.tableView reloadData];
-    }
-  }];
-  // Do any additional setup after loading the view.
-  //    PFQuery *query = [PFQuery queryWithClassName:@"Video"];
-  //    [query getFirstObjectInBackgroundWithBlock:^(PFObject *object, NSError *error) {
-  //        if (object) {
-  //            PFFile *videoFile = [object objectForKey:@"file"];
-  //            NSURL *fileUrl = [NSURL URLWithString:videoFile.url];
-  //            self.player = [[KSVideoPlayerView alloc] initWithFrame:CGRectMake(0, 0, 320, 280) contentURL:fileUrl];
-  //            [self.view addSubview:self.player];
-  //            [self.player play];
-  //            //MPMoviePlayerViewController *movie = [[MPMoviePlayerViewController alloc] initWithContentURL:fileUrl];
-  //            //[self presentMoviePlayerViewControllerAnimated:movie];
-  //        }
-  //    }];
-  
+    
   [self.tableView setAllowsSelection:NO];
+    [self.tableView reloadData];
   
 }
 
@@ -107,7 +101,7 @@
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-  return 385;
+  return 5;
 }
 
 //for each header
@@ -117,9 +111,12 @@
   
   UIColor *descColor = [UIColor colorWithRed:136/255.0f green:136/255.0f blue:136/255.0f alpha:1.0f];
   
+    // get data
+    NSString *username = [self getPerson];
+    NSString *action = [self getAction];
   //setup avatar
   UIImageView *avatarView = [[UIImageView alloc] initWithFrame:CGRectMake(10, 20, 40, 40)];
-  avatarView.image = [UIImage imageNamed:@"tempnewsavatar.png"];
+  avatarView.image = [UIImage imageNamed:[NSString stringWithFormat:@"%@.png", username]];
   [view addSubview:avatarView];
   
   //setup avatar name
@@ -128,7 +125,7 @@
   [avatarName setBackgroundColor:[UIColor clearColor]];
   [avatarName setFont:[UIFont fontWithName:@"Avenir" size:16]];
   
-  avatarName.text = @"lucyguo";
+    avatarName.text = username;//@"lucyguo";
   avatarName.lineBreakMode = NSLineBreakByWordWrapping;
   avatarName.numberOfLines = 0;
   [view addSubview:avatarName];
@@ -139,7 +136,7 @@
   [tags setBackgroundColor:[UIColor clearColor]];
   [tags setFont:[UIFont fontWithName:@"Avenir-Light" size:10]];
   
-  tags.text = @"Surfing with da fox.";
+    tags.text = action;//@"Surfing with da fox.";
   tags.lineBreakMode = NSLineBreakByWordWrapping;
   tags.numberOfLines = 0;
   [view addSubview:tags];
@@ -159,58 +156,6 @@
     cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault  reuseIdentifier:MyIdentifier];
   }
   cell.backgroundColor = [UIColor clearColor];
-  
-  
-  //  PFObject *object = self.videos[indexPath.row];
-  //  PFFile *videoFile = [object objectForKey:@"file"];
-  //  NSURL *fileUrl = [NSURL URLWithString:videoFile.url];
-  //  self.player = [[KSVideoPlayerView alloc] initWithFrame:CGRectMake(0, 0, 320, 280) contentURL:fileUrl];
-  //  [cell addSubview:self.player];
-  //[self.player play];
-  //MPMoviePlayerViewController *movie = [[MPMoviePlayerViewController alloc] initWithContentURL:fileUrl];
-  //[self presentMoviePlayerViewControllerAnimated:movie];
-  
-  UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 320, 320)];
-  imageView.image = [UIImage imageNamed:@"tempsingleimage.png"];
-  [cell addSubview:imageView];
-  
-  UIColor *descColor = [UIColor colorWithRed:136/255.0f green:136/255.0f blue:136/255.0f alpha:1.0f];
-  
-  // setup location icon
-  UIImageView *locationIconView = [[UIImageView alloc] initWithFrame:CGRectMake(13, 345, 11, 17)];
-  locationIconView.image = locationIcon;
-  [cell addSubview:locationIconView];
-  
-  // setup comment button
-  UIButton *commentButton = [UIButton buttonWithType:UIButtonTypeCustom];
-  [commentButton setTitle:@"Comment" forState:UIControlStateNormal];
-  
-  commentButton.frame = CGRectMake(self.view.frame.size.width - 90, 345, 32.5, 22);
-  [commentButton addTarget:self action:@selector(commentTouched) forControlEvents:UIControlEventTouchUpInside];
-  [cell addSubview:commentButton];
-  [commentButton setImage:commentButtonIcon forState:UIControlStateNormal];
-  commentButton.contentMode = UIViewContentModeScaleToFill;
-  
-  // setup heart button
-  UIButton *heartButton = [UIButton buttonWithType:UIButtonTypeCustom];
-  [commentButton setTitle:@"Heart" forState:UIControlStateNormal];
-  
-  heartButton.frame = CGRectMake(self.view.frame.size.width - 40, 345, 32.5, 22);
-  [heartButton addTarget:self action:@selector(heartTouched) forControlEvents:UIControlEventTouchUpInside];
-  [cell addSubview:heartButton];
-  [heartButton setImage:heartButtonIcon forState:UIControlStateNormal];
-  heartButton.contentMode = UIViewContentModeScaleToFill;
-  
-  //setup Location label
-  UILabel *desc = [[UILabel alloc] initWithFrame:CGRectMake(29, 330, 200, 50)];
-  [desc setTextColor:descColor];
-  [desc setBackgroundColor:[UIColor clearColor]];
-  [desc setFont:[UIFont fontWithName:@"Avenir" size:11]];
-  
-  desc.text = @"Mountain View, CA";
-  desc.lineBreakMode = NSLineBreakByWordWrapping;
-  desc.numberOfLines = 0;
-  [cell addSubview:desc];
   
   return cell;
 }

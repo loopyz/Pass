4fb8709775d9554a783bd54774d83e22229929ca
Ivan Wang <iwang@godaddy.com>
2014-07-30 20:22:23 -0700

    Add job to drop inactive pets

--- spire-cloud/cloud/jobs.js
+++ spire-cloud/cloud/jobs.js
@@ -1,6 +1,27 @@
-/* Drop pets that have not changed ownership after seven days. */
+/* Drop pets with inactive users after seven days. */
 Parse.Cloud.job('dropInactivePets', function(request, status) {
-  status.message('Running job: dropInactivePets');
-  // TODO: implement me
-  status.success('0 pets dropped!');
+  status.message('Starting job: dropInactivePets');
+
+  Parse.Cloud.useMasterKey();
+  var INACTIVE_LIMIT = 7; // number of days before pet is dropped
+  var expiration = new Date();
+  expiration.setDate(expiration.getDate() - INACTIVE_LIMIT);
+
+  var query = new Parse.Query(Parse.Pet);
+  query.each(function(pet) {
+    var user = pet.get('currentUser');
+    if (user && user.updatedAt < expiration) {
+      // TODO: Send a message to user telling them their pet was dropped.
+      status.message('Dropping pet: ' + pet.get('name'));
+      pet.set('currentUser', null);
+      pet.increment('passes');
+      return pet.save();
+    }
+    return true;
+  }).then(function() {
+    status.success('Job ran successfully.');
+  }, function(error) {
+    status.error('Error: ' + error.toString());
+  });
+
 });
